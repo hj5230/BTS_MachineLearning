3ca38025dd64b2f4562b6e465981dfdd2fc17f21
hj5230 <hf305@foxmail.com>
2023-08-03 15:59:17 +0800

    rf cross tests in-progress

--- test_rf_input_5f1.sh
+++ test_rf_input_5f1.sh
@@ -6,4 +6,4 @@
     python3 -u "./AdvancedTests/random_forest/input_shaft_cross_validate/cross_5fold_2.py"
     python3 -u "./AdvancedTests/random_forest/input_shaft_cross_validate/cross_5fold_1_2.py"
     python3 -u "./AdvancedTests/random_forest/input_shaft_cross_validate/cross_5fold_2_1.py"
-} | tee "./TestLogs/test_rf_input_5f1.log"
+} | tee "./TestLogs/test_rf_input_5f1_2.log"
