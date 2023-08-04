672a3caa73474049e1a8ac0dfa638c0c65abd24a
hj5230 <hf305@foxmail.com>
2023-08-04 09:16:08 +0800

    revise bash scripts

--- test_gb_input_5f1.sh
+++ test_gb_input_5f1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 {
     echo "Executing Advanced Tests: Gradient Boosting Input Shaft Cross Validate"
-    python3 -u "./AdvancedTests/gradient_boosting/input_shaft_cross_validate/cross_5fold_1.py"
-    python3 -u "./AdvancedTests/gradient_boosting/input_shaft_cross_validate/cross_5fold_2.py"
-    python3 -u "./AdvancedTests/gradient_boosting/input_shaft_cross_validate/cross_5fold_1_2.py"
-    python3 -u "./AdvancedTests/gradient_boosting/input_shaft_cross_validate/cross_5fold_2_1.py"
+    python3 -u "./AdvancedTests/gradient_boosting/input_shaft_cross_validate/cross_5fold_1.py" 2>&1
+    python3 -u "./AdvancedTests/gradient_boosting/input_shaft_cross_validate/cross_5fold_2.py" 2>&1
+    python3 -u "./AdvancedTests/gradient_boosting/input_shaft_cross_validate/cross_5fold_1_2.py" 2>&1
+    python3 -u "./AdvancedTests/gradient_boosting/input_shaft_cross_validate/cross_5fold_2_1.py" 2>&1
 } | tee "./TestLogs/test_gb_input_5f1.log"
