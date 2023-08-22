8723a5100e470cf54b7548d4a831c68ac3641979
hj5230 <hf305@foxmail.com>
2023-08-22 16:23:05 +0800

    remove cnn

--- Executable/test_nn_output_5f1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-{
-    echo "Executing Advanced Tests: Neural Network Output Shaft Cross Validate"
-    python3 -u "./AdvancedTests/neural_network/output_shaft_cross_validate/cross_5fold_1.py" 2>&1
-    python3 -u "./AdvancedTests/neural_network/output_shaft_cross_validate/cross_5fold_2.py" 2>&1
-    python3 -u "./AdvancedTests/neural_network/output_shaft_cross_validate/cross_5fold_1_2.py" 2>&1
-    python3 -u "./AdvancedTests/neural_network/output_shaft_cross_validate/cross_5fold_2_1.py" 2>&1
-} | tee "./TestLogs/test_nn_output_5f1.log"
